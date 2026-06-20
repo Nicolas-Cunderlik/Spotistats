9908b81e4ce8e0da0be65a1b17729d31a87818e7
Nicolas <nicolas7cunderlik@gmail.com>
2026-06-19 20:28:39 -0400

    Fixed title bar and album cover photo

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ Run with: python run.py
 """
 import sys
 import logging
+import pywinstyles
 from PyQt5.QtWidgets import QApplication
 from spotistats.ui.main_window import SpotifyApp
 
@@ -12,6 +13,7 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = SpotifyApp()
+    pywinstyles.apply_style(window, "dark")
     window.show()
     logger.info("Spotistats started")
     sys.exit(app.exec_())
